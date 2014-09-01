dcdd1e598597ebff608c27d1cf948267caba3472
Detective Squirrel <Ratman7539@windowslive.com>
2014-09-01 14:13:38 +1200

    1

--- VeigarEndboss/Program.cs
+++ VeigarEndboss/Program.cs
@@ -71,9 +71,7 @@ namespace VeigarEndboss
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            // Auto stack Q
-            if(menu.SubMenu("misc").Item("miscStackQ").GetValue<bool>() && !menu.SubMenu("combo").Item("comboActive").GetValue<KeyBind>().Active)
-                OnAutoQ();
+
             // Auto W on stunned
             DarkMatter.AutoCastStunned = menu.SubMenu("misc").Item("miscAutoW").GetValue<bool>();
 
@@ -86,6 +84,9 @@ namespace VeigarEndboss
             // WaveClear
             if (menu.SubMenu("waveClear").Item("waveActive").GetValue<KeyBind>().Active)
                 OnWaveClear();
+            // Auto stack Q
+            if(menu.SubMenu("misc").Item("miscStackQ").GetValue<bool>() && !menu.SubMenu("combo").Item("comboActive").GetValue<KeyBind>().Active)
+                OnAutoQ();
         }
 
         private static void OnCombo()
@@ -198,28 +199,64 @@ namespace VeigarEndboss
 
         private static void OnAutoQ()
         {
-			// Auto farm Q minions
-            if (Q.IsReady())
-            {
-                var minions = MinionManager.GetMinions(ObjectManager.Player.Position, spell.Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.MaxHealth);
-                minions.AddRange(MinionManager.GetMinions(ObjectManager.Player.Position, spell.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth));
-                foreach (var minion in minions)
-                {
-                    // Predicted health
-                    float predictedHealth = HealthPrediction.GetHealthPrediction(minion, (int)((minion.Distance(ObjectManager.Player) / spell.Speed) * 1000 + spell.Delay * 1000), 100);
+			private static Spell spell;
+			private static Orbwalking.Orbwalker orbwalker;
 
-                    // Calculated damage on minion
-                    double damage = DamageLib.CalcMagicMinionDmg((35 + (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Q).Level * 45)) + (0.60 * ObjectManager.Player.FlatMagicDamageMod), minion as Obj_AI_Minion, true);
+			private static bool autoFarmMinions = false;
+			private static int lastNetworkId = -1;
 
-                    // Valid minion
-                    if (predictedHealth > 0 && damage > predictedHealth)
-                    {
-                        spell.CastOnUnit(minion);
-                        lastNetworkId = minion.NetworkId;
-                        break;
-                    }
-                }
-            }
+			public static bool AutoFarmMinions
+			{
+				get { return autoFarmMinions; }
+				set { autoFarmMinions = value; }
+			}
+
+			public static void Initialize(Spell spell, Orbwalking.Orbwalker orbwalker)
+			{
+				if (BalefulStrike.spell != null)
+					return;
+
+				// Apply values
+				BalefulStrike.spell = spell;
+				BalefulStrike.orbwalker = orbwalker;
+
+				// Register events
+				Game.OnGameUpdate += Game_OnGameUpdate;
+				Orbwalking.BeforeAttack += Orbwalking_BeforeAttack;
+			}
+
+			static void Orbwalking_BeforeAttack(Orbwalking.BeforeAttackEventArgs args)
+			{
+				// Deny attacking on Q'ed minions
+				if (args.Target.NetworkId == lastNetworkId)
+					args.Process = false;
+			}
+
+			private static void Game_OnGameUpdate(EventArgs args)
+			{
+				// Auto farm Q minions
+				if (spell.IsReady() && Orbwalking.CanMove(100))
+				{
+					var minions = MinionManager.GetMinions(ObjectManager.Player.Position, spell.Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.MaxHealth);
+					minions.AddRange(MinionManager.GetMinions(ObjectManager.Player.Position, spell.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth));
+					foreach (var minion in minions)
+					{
+						// Predicted health
+						float predictedHealth = HealthPrediction.GetHealthPrediction(minion, (int)((minion.Distance(ObjectManager.Player) / spell.Speed) * 1000 + spell.Delay * 1000), 100);
+
+						// Calculated damage on minion
+						double damage = DamageLib.CalcMagicMinionDmg((35 + (ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Q).Level * 45)) + (0.60 * ObjectManager.Player.FlatMagicDamageMod), minion as Obj_AI_Minion, true);
+
+						// Valid minion
+						if (predictedHealth > 0 && damage > predictedHealth)
+						{
+							spell.CastOnUnit(minion);
+							lastNetworkId = minion.NetworkId;
+							break;
+						}
+					}
+				}
+			}
         }
 
         private static void Drawing_OnDraw(EventArgs args)
